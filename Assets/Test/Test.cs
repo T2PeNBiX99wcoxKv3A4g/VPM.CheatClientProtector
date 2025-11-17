3c9642a66a20ac6d02d7d8086556e5c62d94cec5
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-17 14:25:19 +0800

    fix: Upload files

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using io.github.ykysnk.CheatClientProtector;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.UdonNetworkCalling;
+using VRC.Udon.Common.Interfaces;
+
+namespace Test
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class Test : CheatClientProtectorBehaviour
+    {
+        protected override float InteractDistance => 3;
+
+        public override void Interact() => InteractCheck();
+
+        protected override void InteractAntiCheat()
+        {
+            AnyOneCanCall("Interact Test");
+
+            OnlyCanBeCallFromInteract("Interact Test", RandomKey);
+
+            RequestCallMethod(nameof(InteractAntiCheat), nameof(OnlyCanBeCallFromInteractWithDelayed));
+            SendCustomEventDelayedSeconds(nameof(OnlyCanBeCallFromInteractWithDelayed), 2);
+
+            RequestKeyCheckSync();
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(OnlyCanBeCallFromInteractWithNetwork));
+
+            RequestKeyCheckSync();
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(OnlyCanBeCallFromInteractWithNetwork2), "Interact Test");
+        }
+
+        // Always can be called from run custom event. you can just call it from inspector.
+        [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Global")]
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public void AnyOneCanCall(string someTestArg)
+        {
+            Debug.Log($"AnyOneCanCall {someTestArg}");
+        }
+
+        // Only can be called from interacting.
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public void OnlyCanBeCallFromInteract(string someTestArg, int key)
+        {
+            if (!IsKeyCorrect(key)) return;
+            Debug.Log($"OnlyCanBeCallFromInteract {someTestArg} {key}");
+        }
+
+        public void OnlyCanBeCallFromInteractWithDelayed()
+        {
+            if (!IsMethodHaveRequest(nameof(InteractAntiCheat), nameof(OnlyCanBeCallFromInteractWithDelayed))) return;
+            Debug.Log("OnlyCanBeCallFromInteractWithDelayed");
+        }
+
+        public void OnlyCanBeCallFromInteractWithNetwork()
+        {
+            if (!IsKeyCorrectSync()) return;
+            Debug.Log("OnlyCanBeCallFromInteractWithNetwork");
+        }
+
+        [NetworkCallable]
+        public void OnlyCanBeCallFromInteractWithNetwork2(string someTestArg)
+        {
+            if (!IsKeyCorrectSync()) return;
+            Debug.Log($"OnlyCanBeCallFromInteractWithNetwork {someTestArg}");
+        }
+    }
+}
---
+++ Packages/io.github.ykysnk.cheat-client-protector/Runtime/CheatClientProtectorBehaviour.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using io.github.ykysnk.utils.Extensions;
+using JetBrains.Annotations;
+using UdonSharp;
+using VRC.SDK3.Data;
+using VRC.SDKBase;
+using Random = System.Random;
+
+namespace io.github.ykysnk.CheatClientProtector
+{
+    [PublicAPI]
+    public abstract class CheatClientProtectorBehaviour : UdonSharpBehaviour
+    {
+        protected virtual float InteractDistance => 3;
+
+        private readonly DataDictionary _keyCheckPool = new DataDictionary();
+        private int _keyCheck;
+        [UdonSynced] private int _keyCheckSync;
+        private int _randomKey;
+
+        protected int RandomKey
+        {
+            get
+            {
+                var random = new Random(DateTime.Now.Millisecond * _randomKey);
+                return _randomKey = random.Next(int.MinValue, int.MaxValue);
+            }
+        }
+
+        [SuppressMessage("ReSharper", "UnusedVariable")]
+        protected bool IsKeyCorrect(int key)
+        {
+            var correct = key == _randomKey;
+            var newKey = RandomKey;
+            return correct;
+        }
+
+        protected bool IsKeyCorrect()
+        {
+            var keyCheck = _keyCheck;
+            _keyCheck = 0;
+            return IsKeyCorrect(keyCheck);
+        }
+
+        protected bool IsKeyCorrectSync()
+        {
+            var keyCheck = _keyCheckSync;
+            _keyCheckSync = 0;
+            return IsKeyCorrect(keyCheck);
+        }
+
+        protected bool IsMethodHaveRequest(string senderMethodName, string receiverMethodName)
+        {
+            // Kinda useless, I'm lazy to convert it to sha256
+            senderMethodName = Base64(senderMethodName);
+            receiverMethodName = Base64(receiverMethodName);
+
+            if (!_keyCheckPool.TryGetValue(receiverMethodName, TokenType.DataDictionary, out var listToken))
+                return false;
+
+            var dictionary = listToken.DataDictionary;
+
+            if (!dictionary.TryGetValue(senderMethodName, out var valueToken))
+                return false;
+
+            dictionary[senderMethodName] = valueToken.Int - 1;
+
+            if (dictionary[senderMethodName].Int < 1)
+                dictionary.Remove(senderMethodName);
+            return true;
+        }
+
+        // Anyone can fake the senderMethodName, but this method is not public, so maybe it's safe.
+        protected void RequestCallMethod(string senderMethodName, string receiverMethodName)
+        {
+            // Kinda useless, I'm lazy to convert it to sha256
+            senderMethodName = Base64(senderMethodName);
+            receiverMethodName = Base64(receiverMethodName);
+
+            if (!_keyCheckPool.TryGetValue(receiverMethodName, TokenType.DataDictionary, out var listToken))
+            {
+                _keyCheckPool.Add(receiverMethodName, new DataDictionary());
+                listToken = _keyCheckPool[receiverMethodName];
+            }
+
+            var dictionary = listToken.DataDictionary;
+
+            if (!dictionary.TryGetValue(senderMethodName, out var valueToken))
+            {
+                dictionary.Add(senderMethodName, 0);
+                valueToken = dictionary[senderMethodName];
+            }
+
+            dictionary[senderMethodName] = valueToken.Int + 1;
+        }
+
+        protected void RequestKeyCheck() => _keyCheck = RandomKey;
+
+        protected void RequestKeyCheckSync() => _keyCheckSync = RandomKey;
+
+        protected string Base64(object value) => Convert.ToBase64String(Encoding.UTF8.GetBytes(value.ToString()));
+
+        protected void InteractCheck()
+        {
+            if (!Utilities.IsValid(gameObject) || !gameObject.IsPlayerCloseRange(InteractDistance)) return;
+            InteractAntiCheat();
+        }
+
+        protected void InteractCheck2D()
+        {
+            if (!Utilities.IsValid(gameObject) || !gameObject.IsPlayerCloseRange2D(InteractDistance)) return;
+            InteractAntiCheat();
+        }
+
+        protected virtual void InteractAntiCheat()
+        {
+        }
+    }
+}
